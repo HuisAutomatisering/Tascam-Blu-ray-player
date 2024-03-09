e07e7b5d3243ea34f000fccb191202c179aae7b3
HuisAutomatisering <161373902+HuisAutomatisering@users.noreply.github.com>
2024-03-09 15:08:00 +0100

    Update media_player.py

--- media_player.py
+++ media_player.py
@@ -1,21 +1,20 @@
-# media_player.py
 import logging
 
-from homeassistant.components.media_player import MediaPlayerEntity
+from homeassistant.components.media_player import (
+    MediaPlayerEntity,
+    MediaPlayerEntity
+)
+from homeassistant.const import STATE_OFF, STATE_PLAYING, STATE_PAUSED, STATE_IDLE
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "tascam_bd_mp4k"
-
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    async_add_entities([TascamBdMp4kMediaPlayer()])
-
-
 class TascamBdMp4kMediaPlayer(MediaPlayerEntity):
-    def __init__(self):
-        self._name = "TASCAM BD-MP4K"
+    def __init__(self, host, name):
+        self._host = host
+        self._name = name
         self._state = None
+        self._media_position = 0
+        self._media_duration = 0
 
     @property
     def name(self):
@@ -26,45 +25,67 @@ class TascamBdMp4kMediaPlayer(MediaPlayerEntity):
         return self._state
 
     @property
-    def supported_features(self):
-        return 0
+    def media_position(self):
+        return self._media_position
 
-    def turn_on(self):
-        # Implement logic to send command to power on the device
-        _LOGGER.debug("Turning on TASCAM BD-MP4K")
-
-    def turn_off(self):
-        # Implement logic to send command to power off the device
-        _LOGGER.debug("Turning off TASCAM BD-MP4K")
-
-    def media_play(self):
-        # Implement logic to send command to play media
-        _LOGGER.debug("Playing media on TASCAM BD-MP4K")
-
-    def media_pause(self):
-        # Implement logic to send command to pause media
-        _LOGGER.debug("Pausing media on TASCAM BD-MP4K")
-
-    def media_stop(self):
-        # Implement logic to send command to stop media
-        _LOGGER.debug("Stopping media on TASCAM BD-MP4K")
-
-    def volume_up(self):
-        # Implement logic to increase volume
-        _LOGGER.debug("Increasing volume on TASCAM BD-MP4K")
-
-    def volume_down(self):
-        # Implement logic to decrease volume
-        _LOGGER.debug("Decreasing volume on TASCAM BD-MP4K")
-
-    def mute_volume(self, mute):
-        # Implement logic to mute/unmute volume
-        _LOGGER.debug("Muting/unmuting volume on TASCAM BD-MP4K")
-
-    def select_source(self, source):
-        # Implement logic to select input source
-        _LOGGER.debug("Selecting input source on TASCAM BD-MP4K")
+    @property
+    def media_duration(self):
+        return self._media_duration
 
     def update(self):
-        # Implement logic to update state of the media player
-        _LOGGER.debug("Updating state of TASCAM BD-MP4K media player")
+        """Update the state of the media player."""
+        # Implement logic to query the device and update state
+        try:
+            # Example: query the device for current state
+            # and update self._state, self._media_position, self._media_duration
+            self._state = STATE_PLAYING  # Placeholder, replace with actual state
+            self._media_position = 0  # Placeholder, replace with actual position
+            self._media_duration = 3600  # Placeholder, replace with actual duration
+        except Exception as e:
+            _LOGGER.error("Error updating TASCAM BD-MP4K media player state: %s", e)
+            self._state = None
+
+    def turn_on(self):
+        """Turn on the media player."""
+        # Implement logic to send command to power on the device
+        try:
+            # Example: send command to power on the device
+            _LOGGER.debug("Turning on TASCAM BD-MP4K")
+        except Exception as e:
+            _LOGGER.error("Error turning on TASCAM BD-MP4K: %s", e)
+
+    def turn_off(self):
+        """Turn off the media player."""
+        # Implement logic to send command to power off the device
+        try:
+            # Example: send command to power off the device
+            _LOGGER.debug("Turning off TASCAM BD-MP4K")
+        except Exception as e:
+            _LOGGER.error("Error turning off TASCAM BD-MP4K: %s", e)
+
+    def media_play(self):
+        """Play media."""
+        # Implement logic to send command to play media
+        try:
+            # Example: send command to play media
+            _LOGGER.debug("Playing media on TASCAM BD-MP4K")
+        except Exception as e:
+            _LOGGER.error("Error playing media on TASCAM BD-MP4K: %s", e)
+
+    def media_pause(self):
+        """Pause media."""
+        # Implement logic to send command to pause media
+        try:
+            # Example: send command to pause media
+            _LOGGER.debug("Pausing media on TASCAM BD-MP4K")
+        except Exception as e:
+            _LOGGER.error("Error pausing media on TASCAM BD-MP4K: %s", e)
+
+    def media_stop(self):
+        """Stop media."""
+        # Implement logic to send command to stop media
+        try:
+            # Example: send command to stop media
+            _LOGGER.debug("Stopping media on TASCAM BD-MP4K")
+        except Exception as e:
+            _LOGGER.error("Error stopping media on TASCAM BD-MP4K: %s", e)
