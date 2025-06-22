fe9c320a4fd9b9ffeb7228fb772a9c464edab305
HuisAutomatisering <161373902+HuisAutomatisering@users.noreply.github.com>
2025-06-22 21:14:43 +0200

    Update media_player.py

--- media_player.py
+++ media_player.py
@@ -1,129 +1,66 @@
-"""
-TASCAM BD-MP4K Media Player Integration for Home Assistant
-"""
-import asyncio
-import logging
 import socket
-
+import logging
 from homeassistant.components.media_player import (
     MediaPlayerEntity,
-    PLATFORM_SCHEMA,
+    MediaPlayerEntityFeature,
+    MediaType,
 )
-from homeassistant.const import (
-    STATE_IDLE,
-    STATE_OFF,
-    STATE_PAUSED,
-    STATE_PLAYING,
-)
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
+from homeassistant.const import STATE_PLAYING, STATE_PAUSED, STATE_IDLE, STATE_UNKNOWN
+
+from .const import DOMAIN, CONF_IP_ADDRESS, CONF_PORT, COMMANDS
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'tascam_bd_mp4k'
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    ip = config_entry.data[CONF_IP_ADDRESS]
+    port = config_entry.data[CONF_PORT]
+    player = TascamBDMP4KMediaPlayer(ip, port)
+    _LOGGER.info("Setting up Tascam BD-MP4K media player entity.")
+    async_add_entities([player], update_before_add=True)
 
-CONF_HOST = 'host'
-CONF_PORT = 'port'
+class TascamBDMP4KMediaPlayer(MediaPlayerEntity):
+    _attr_supported_features = (
+        MediaPlayerEntityFeature.PLAY
+        | MediaPlayerEntityFeature.PAUSE
+        | MediaPlayerEntityFeature.NEXT_TRACK
+        | MediaPlayerEntityFeature.PREVIOUS_TRACK
+    )
+    _attr_media_content_type = MediaType.VIDEO
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_PORT, default=9030): cv.port
-})
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the TASCAM media player platform."""
-    host = config[CONF_HOST]
-    port = config[CONF_PORT]
-
-    # Create TASCAM media player instance
-    player = TascamMediaPlayer(host, port)
-
-    # Add the media player entity
-    async_add_entities([player])
-
-class TascamMediaPlayer(MediaPlayerEntity):
-    """Representation of a TASCAM media player."""
-
-    def __init__(self, host, port):
-        """Initialize the TASCAM media player."""
-        self._host = host
+    def __init__(self, ip, port):
+        self._ip = ip
         self._port = port
-        self._state = STATE_OFF
-        self._name = "TASCAM Media Player"
-        self._socket = None
+        self._attr_name = "Tascam BD-MP4K"
+        self._attr_unique_id = f"tascam_{ip.replace('.', '_')}_{port}"
+        self._attr_state = STATE_IDLE
+
+    def _send_command(self, command):
+        try:
+            with socket.create_connection((self._ip, self._port), timeout=5) as sock:
+                sock.sendall(command.encode())
+                response = sock.recv(1024).decode().strip()
+                _LOGGER.debug("Sent: %s | Received: %s", command.strip(), response)
+                return response
+        except Exception as e:
+            _LOGGER.error("Tascam command error: %s", e)
+            return None
 
     async def async_update(self):
-        """Update the state of the media player."""
-        # Implement update logic here if needed
+        # Geen polling implementatie voor nu
         pass
 
-    async def async_turn_on(self):
-        """Turn on the media player."""
-        await self._send_command("!7PWR01")
-        self._state = STATE_IDLE
-
-    async def async_turn_off(self):
-        """Turn off the media player."""
-        await self._send_command("!7PWR00")
-        self._state = STATE_OFF
-
     async def async_media_play(self):
-        """Send play command."""
-        await self._send_command("!7PLY")
-        self._state = STATE_PLAYING
+        self._send_command(COMMANDS["play"])
+        self._attr_state = STATE_PLAYING
+        self.async_write_ha_state()
 
     async def async_media_pause(self):
-        """Send pause command."""
-        await self._send_command("!7PAS")
-        self._state = STATE_PAUSED
+        self._send_command(COMMANDS["pause"])
+        self._attr_state = STATE_PAUSED
+        self.async_write_ha_state()
 
-    async def async_added_to_hass(self):
-        """Handle when an entity is added to Home Assistant."""
-        await self._connect()
+    async def async_media_next_track(self):
+        self._send_command(COMMANDS["next"])
 
-    async def async_will_remove_from_hass(self):
-        """Handle when an entity is removed from Home Assistant."""
-        if self._socket:
-            self._socket.close()
-            self._socket = None
-
-    async def _connect(self):
-        """Establish a connection to the TASCAM media player."""
-        if self._socket is None:
-            self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            await self.hass.async_add_executor_job(self._socket.connect, (self._host, self._port))
-
-    async def _send_command(self, command):
-        """Send command to the TASCAM media player."""
-        try:
-            await self._connect()
-            await self.hass.async_add_executor_job(self._socket.send, command.encode())
-        except (ConnectionError, OSError) as err:
-            _LOGGER.error("Error communicating with TASCAM media player: %s", err)
-            self._socket = None
-            raise
-
-    @property
-    def name(self):
-        """Return the name of the media player."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the media player."""
-        return self._state
-
-    @property
-    def available(self):
-        """Return True if the media player is available."""
-        return self._socket is not None
-
-    @property
-    def supported_features(self):
-        """Flag media player features that are supported."""
-        return (
-            SUPPORT_TURN_ON |
-            SUPPORT_TURN_OFF |
-            SUPPORT_PLAY |
-            SUPPORT_PAUSE
-        )
+    async def async_media_previous_track(self):
+        self._send_command(COMMANDS["previous"])
